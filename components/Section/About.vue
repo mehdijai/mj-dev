39ed4b5cb43445c3749201a574115cf3fe083a75
mehd ai <contact@medostudios.com>
2023-05-09 23:10:01 +0100

    style about section

--- components/Section/About.vue
+++ components/Section/About.vue
@@ -28,7 +28,7 @@ const skills = [
 </script>
 
 <template>
-    <section class="about" id="about">
+    <section class="about section" id="about">
         <h2>About me</h2>
         <p>
             <strong>Mehdi Jai, VueJS Frontend web developer.</strong> I'm a curious, autonomous developer. I started my journey in 2019
@@ -43,3 +43,33 @@ const skills = [
         </div>
     </section>
 </template>
+
+<style lang="scss">
+.about {
+    min-height: 100vh;
+    .skills {
+        width: $section-content-width;
+        display: flex;
+        flex-wrap: wrap;
+        gap: 10px;
+        .skill {
+            color: $white;
+            background: darken($green-dark, 10);
+            font-weight: 700;
+            padding: 5px 8px;
+            font-size: 13px;
+            border-radius: 5px;
+            box-shadow: 0 0 20px rgba($black, 0.3);
+            user-select: none;
+            transition: 0.4s ease-in-out;
+            cursor: pointer;
+            
+            &:hover {
+                color: $green-dark;
+                background: $white;
+                box-shadow: 0 0 20px rgba($green-light, 0.9);
+            }
+        }
+    }
+}
+</style>
