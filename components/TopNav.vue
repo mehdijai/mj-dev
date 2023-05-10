f42282d946ac35a394b467c6106f9db0d7587043
mehd ai <contact@medostudios.com>
2023-05-10 06:48:54 +0100

    top nav animation

--- components/TopNav.vue
+++ components/TopNav.vue
@@ -1,21 +1,40 @@
 <script setup lang="ts">
+  import { gsap } from "gsap";
+  const openMenu = ref(false);
+  onMounted(() => {
+    const tl = gsap.timeline({
+      defaults: { duration: 0.5, ease: "power4.inOut" },
+    });
+    tl.to(".top-nav", {
+      opacity: 1,
+    }).from(
+      ".top-nav ul > li",
+      {
+        y: -5,
+        opacity: 0,
+        stagger: 0.1,
+      },
+      "-=0.3",
+    );
+  });
 </script>
 
 <template>
-    <nav class="top-nav">
-        <ul>
-            <li class="logo">
-                <NuxtLink to="/">
-                    <img src="/logo.svg" alt="MJDev">
-                </NuxtLink>
-            </li>
-            <NavigationLinks />
-        </ul>
-    </nav>
+  <nav class="top-nav">
+    <ul>
+      <li class="logo">
+        <NuxtLink to="/">
+          <img src="/logo.svg" alt="MJDev" />
+        </NuxtLink>
+      </li>
+      <NavigationLinks />
+    </ul>
+  </nav>
 </template>
 
 <style lang="scss">
-.top-nav {
+  .top-nav {
+    opacity: 0;
     z-index: 80;
     position: fixed;
     background-color: $glass-white;
@@ -24,21 +43,21 @@
     margin-left: calc(22px);
 
     .logo {
-        margin-right: auto;
+      margin-right: auto;
 
-        img {
-            height: 30px;
-        }
+      img {
+        height: 30px;
+      }
     }
 
     ul {
-        max-width: 1000px;
-        padding: 0;
-        margin: 0 auto;
-        display: flex;
-        align-items: center;
-        padding: 0 20px;
-        gap: 10px;
+      max-width: 1000px;
+      padding: 0;
+      margin: 0 auto;
+      display: flex;
+      align-items: center;
+      padding: 0 20px;
+      gap: 10px;
     }
-}
+  }
 </style>
