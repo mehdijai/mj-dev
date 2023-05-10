96d0d734b326e30ecfaf21579664f39005ad6539
mehd ai <contact@medostudios.com>
2023-05-10 18:01:09 +0100

    About section animation

--- components/Section/About.vue
+++ components/Section/About.vue
@@ -1,75 +1,115 @@
 <script setup lang="ts">
-const skills = [
-"HTML",
-"CSS",
-"JS",
-"TailwindCSS",
-"SCSS/SASS",
-"GSAP",
-"VueJS",
-"NuxtJS",
-"AlpineJS",
-"ES6",
-"TypeScript",
-"AstroJS",
-"NodeJS",
-"ExpressJS",
-"PrismaJS",
-"PHP",
-"Laravel",
-"SQL",
-"Git",
-"Github",
-"Bitbucket",
-"Jira",
-"MS DevOps",
-"Postman",
-]
+  import { gsap } from "gsap";
+  import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+  const skills = [
+    "HTML",
+    "CSS",
+    "JS",
+    "TailwindCSS",
+    "SCSS/SASS",
+    "GSAP",
+    "VueJS",
+    "NuxtJS",
+    "AlpineJS",
+    "ES6",
+    "TypeScript",
+    "AstroJS",
+    "NodeJS",
+    "ExpressJS",
+    "PrismaJS",
+    "PHP",
+    "Laravel",
+    "SQL",
+    "Git",
+    "Github",
+    "Bitbucket",
+    "Jira",
+    "MS DevOps",
+    "Postman",
+  ];
+
+  onMounted(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    const tl = gsap.timeline({
+      defaults: { duration: 0.8, ease: "power4.inOut" },
+      scrollTrigger: ".about",
+    });
+    tl.from(".about h2", {
+      opacity: 0,
+      x: -10,
+    }).from(
+      ".about p",
+      {
+        opacity: 0,
+        y: -50,
+      },
+      "-=0.5",
+    );
+    ScrollTrigger.batch(".skill", {
+      duration: 0.4,
+      onEnter: (elements) =>
+        gsap.to(elements, {
+          opacity: 1,
+          clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+          stagger: 0.1,
+        }),
+      onLeaveBack: (elements) =>
+        gsap.set(elements, {
+          opacity: 0,
+          clipPath: "polygon(0% 0%, 0% 0%, 0% 100%, 0% 100%)",
+          overwrite: true,
+        }),
+    });
+  });
 </script>
 
 <template>
-    <section class="about section" id="about">
-        <h2>About me</h2>
-        <p>
-            <strong>Mehdi Jai, VueJS Frontend web developer.</strong> I'm a curious, autonomous developer. I started my journey in 2019
-            professionally. The beginning was with building my two majors SaaS; Tabarro3.ma, Autodrive.ma. I have a long
-            experience with JavaScript/TypeScript, SCSS/SASS, NodeJS, and PHP Laravel, VueJS. Plus, ElectronJS, VBA,
-            Figma, and other tools, programming languages, and skills.
-        </p>
-        <div class="skills">
-            <template v-for="skill in skills" :key="skill">
-                <span class="skill">{{ skill }}</span>
-            </template>
-        </div>
-    </section>
+  <section class="about section" id="about">
+    <h2>About me</h2>
+    <p>
+      <strong>Mehdi Jai, VueJS Frontend web developer.</strong> I'm a curious, autonomous developer.
+      I started my journey in 2019 professionally. The beginning was with building my two majors
+      SaaS; Tabarro3.ma, Autodrive.ma. I have a long experience with JavaScript/TypeScript,
+      SCSS/SASS, NodeJS, and PHP Laravel, VueJS. Plus, ElectronJS, VBA, Figma, and other tools,
+      programming languages, and skills.
+    </p>
+    <div class="skills">
+      <template v-for="skill in skills" :key="skill">
+        <span class="skill">{{ skill }}</span>
+      </template>
+    </div>
+  </section>
 </template>
 
 <style lang="scss">
-.about {
+  .about {
     min-height: 100vh;
     .skills {
-        width: $section-content-width;
-        display: flex;
-        flex-wrap: wrap;
-        gap: 10px;
-        .skill {
-            color: $white;
-            background: darken($green-dark, 10);
-            font-weight: 700;
-            padding: 5px 8px;
-            font-size: 13px;
-            border-radius: 5px;
-            box-shadow: $default-shadow;
-            user-select: none;
-            transition: 0.4s ease-in-out;
-            cursor: pointer;
-            
-            &:hover {
-                color: $green-dark;
-                background: $white;
-                box-shadow: $green-glow;
-            }
+      width: $section-content-width;
+      display: flex;
+      flex-wrap: wrap;
+      gap: 10px;
+      .skill {
+        color: $white;
+        background: darken($green-dark, 10);
+        font-weight: 700;
+        padding: 5px 8px;
+        font-size: 13px;
+        border-radius: 5px;
+        box-shadow: $default-shadow;
+        user-select: none;
+        transition: 0.4s ease-in-out;
+        cursor: pointer;
+        clip-path: polygon(0 0, 0 0, 0 100%, 0 100%);
+
+        &:hover {
+          color: $green-dark;
+          background: $white;
+          box-shadow: $green-glow;
         }
+      }
     }
-}
+  }
 </style>
