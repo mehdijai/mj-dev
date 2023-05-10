347ce796a2955fad440b8f4599ce83101586b642
mehd ai <contact@medostudios.com>
2023-05-10 07:45:40 +0100

    Typewriter animation

--- components/TypeWriter.vue
+++ components/TypeWriter.vue
@@ -1,15 +1,35 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import { gsap, SteppedEase } from "gsap";
+
+  onMounted(() => {
+    const tl = gsap.timeline({
+      defaults: { duration: 1, ease: SteppedEase.config(20) },
+    });
+    tl.fromTo(
+      ".anim-typewriter",
+      {
+        width: 0,
+      },
+      {
+        width: 340,
+      },
+    );
+    tl.fromTo(
+      ".anim-typewriter",
+      {
+        duration: 0.5,
+        "border-right-color": "rgba(255,255,255,0.75)",
+      },
+      {
+        "border-right-color": "rgba(255,255,255,0)",
+        repeat: -1,
+      },
+    );
+  });
+</script>
 
 <template>
-  <div class="type-writer font-game">
-    <span>“</span>
-    <span>H</span>
-    <span>E</span>
-    <span>L</span>
-    <span>L</span>
-    <span>O</span>
-    <span>”</span>
-  </div>
+  <p class="type-writer font-game anim-typewriter">“HELLO”</p>
 </template>
 
 <style lang="scss">
@@ -17,29 +37,33 @@
     color: $green-light;
     font-size: 50px;
     position: relative;
-    width: fit-content;
+    width: 0;
+    overflow: hidden;
     padding-right: 10px;
+    border-right: 2px solid rgba(255, 255, 255, 0.75);
+    box-sizing: content-box;
     display: flex;
     align-items: flex-end;
-    &::after {
-      content: "";
-      position: absolute;
-      right: 0;
-      top: 50%;
-      height: 100%;
-      width: 5px;
-      background-color: $white;
-      transform-origin: center center;
-      transform: translateY(-50%);
-      animation: $cursor-flash;
-    }
+    // background-color: red;
+    // &::after {
+    //   content: "";
+    //   position: absolute;
+    //   right: 0;
+    //   top: 50%;
+    //   height: 100%;
+    //   width: 5px;
+    //   background-color: $white;
+    //   transform-origin: center center;
+    //   transform: translateY(-50%);
+    //   animation: $cursor-flash;
+    // }
     span {
       display: block;
       user-select: none;
       line-height: 1;
-      &:nth-of-type(2){
-        clip-path: $clip-scaled-down;
-      }
+      // &:nth-of-type(2) {
+      //   clip-path: polygone(0 100%, 100% 100%, 100% 100%, 0 100%);
+      // }
     }
   }
 </style>
