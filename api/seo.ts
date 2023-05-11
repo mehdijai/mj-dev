1a415664a93f09a196fd48d26ecdc0e8d19b4c66
mehd ai <contact@medostudios.com>
2023-05-11 09:47:48 +0100

    Add host url to images

--- api/seo.ts
+++ api/seo.ts
@@ -4,8 +4,8 @@ export const seoData = {
     "Mehdi Jai, VueJS Frontend Developer, and UI Designer. I create beautiful interactive interfaces for SaaS', landing pages, and wherever I can write CSS.",
   author: "Mehdi Jai",
   hostUrl: "https://mehdijai.com",
-  coverImage: "/cover.png",
+  coverImage: "https://mehdijai.com/cover.png",
   twitterCreatorId: "jai_mehdi",
   dateCreated: "2023-05-11",
-  dateModified: "2023-05-11"
+  dateModified: "2023-05-11",
 };
